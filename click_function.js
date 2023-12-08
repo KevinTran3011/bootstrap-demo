5e46a3a691622984a99c4838a41d7dc5dc7a47fa
KevinTran3011 <tuankiettran30112000@gmail.com>
2023-12-08 14:39:27 +0700

    added in code for the drop down function on the side bar

--- click_function.js
+++ click_function.js
@@ -1,18 +1,50 @@
-const sidebarSectionOnclick = () => {
-  const targetElement = document.getElementById("user");
+const sidebarSectionOnclick = (event) => {
+  const targetElement = event.currentTarget;
   targetElement.classList.toggle("clicked");
+
+  const downArrow = targetElement.querySelector("#expanded_arrow");
+  if (downArrow) {
+    downArrow.classList.toggle("expanded");
+  }
 };
 
-document
-  .getElementById("user")
-  .addEventListener("click", sidebarSectionOnclick);
+const sectionItems = document.querySelectorAll(".section_item");
+sectionItems.forEach((item) => {
+  item.addEventListener("click", sidebarSectionOnclick);
+});
 
 // FUNCTION FOR GRID ON CLICK
 const gridSectionOnClick = () => {
-  const gridSection = document.querySelector(".box_grid_text");
+  const gridSection = event.currentTarget;
   gridSection.classList.toggle("grid_onClick");
 };
 
-document
-  .querySelector(".box_grid_text")
-  .addEventListener("click", gridSectionOnClick);
+const gridItems = document.querySelectorAll(".box_grid_text");
+gridItems.forEach((item) => {
+  item.addEventListener("click", gridSectionOnClick);
+});
+
+// FUNCTION FOR SIDE BAR LIST
+const toggleCards = () => {
+  var styleList = document.getElementById("cardList");
+  if (styleList) {
+    styleList.style.display =
+      styleList.style.display === "block" ? "none" : "block";
+  }
+};
+
+const toggleProfile = () => {
+  var styleList = document.getElementById("profileList");
+  if (styleList) {
+    styleList.style.display =
+      styleList.style.display === "block" ? "none" : "block";
+  }
+};
+
+const toggleUserDropdown = () => {
+  var userDropdown = document.getElementById("userDropdown");
+  if (userDropdown) {
+    userDropdown.style.display =
+      userDropdown.style.display === "block" ? "none" : "block";
+  }
+};
