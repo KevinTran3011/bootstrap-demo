069c01f4c7a7aeed425dd940383c77038d449fbc
KevinTran3011 <tuankiettran30112000@gmail.com>
2023-12-15 09:21:31 +0700

    fixed the section_item click item the second time

--- click_function.js
+++ click_function.js
@@ -1,4 +1,16 @@
 const sidebarSectionOnclick = (event) => {
+  const targetElement = event.currentTarget;
+
+  // If the clicked element is already selected, deselect it and return
+  if (targetElement.classList.contains("clicked")) {
+    targetElement.classList.remove("clicked");
+    const downArrow = targetElement.querySelector("#expanded_arrow");
+    if (downArrow) {
+      downArrow.classList.remove("expanded");
+    }
+    return;
+  }
+
   // Remove 'clicked' class from all section items
   sectionItems.forEach((item) => {
     item.classList.remove("clicked");
@@ -9,7 +21,6 @@ const sidebarSectionOnclick = (event) => {
   });
 
   // Add 'clicked' class to the clicked section item
-  const targetElement = event.currentTarget;
   targetElement.classList.add("clicked");
 
   const downArrow = targetElement.querySelector("#expanded_arrow");
@@ -25,14 +36,20 @@ sectionItems.forEach((item) => {
 
 // FUNCTION FOR GRID ON CLICK
 const gridSectionOnClick = (event) => {
+  isSelected = false;
   const gridSection = event.currentTarget;
-  gridSection.classList.toggle("grid_onClick");
+  if (!isSelected) {
+    gridSection.classList.toggle("grid_onClick");
 
-  // Toggle text color within the clicked element
-  const textElement = gridSection.querySelector(".material-symbols-outlined");
-  const textElement2 = gridSection.querySelector(".box_grid_text_title");
-  textElement.classList.toggle("text_onClick");
-  textElement2.classList.toggle("text_onClick");
+    // Toggle text color within the clicked element
+    const textElement = gridSection.querySelector(".material-symbols-outlined");
+    const textElement2 = gridSection.querySelector(".box_grid_text_title");
+    textElement.classList.toggle("text_onClick");
+    textElement2.classList.toggle("text_onClick");
+    isSelected = true;
+  } else {
+    grid;
+  }
 };
 
 const gridItems = document.querySelectorAll(".box_grid_text");
