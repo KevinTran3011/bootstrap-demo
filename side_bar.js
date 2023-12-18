66320aa3821e637af4e2e7b2872f9c8d43439325
KevinTran3011 <tuankiettran30112000@gmail.com>
2023-12-18 16:12:28 +0700

    fixed the side bar transition function

--- side_bar.js
+++ side_bar.js
@@ -2,8 +2,8 @@ const showSidebar = () => {
   let width = window.innerWidth;
   if (width < 768) {
     const sidebarSection = document.querySelector(".side_bar");
-    sidebarSection.style.display = "flex";
-    sidebarSection.style.flexDirection = "column";
+    sidebarSection.style.transition = "all 0.2s ease-in-out";
+    sidebarSection.style.width = "50%";
     const closeButton = document.getElementById("closeButton");
     closeButton.style.display = "block";
   }
@@ -11,5 +11,8 @@ const showSidebar = () => {
 
 const hideSidebar = () => {
   const sidebarSection = document.querySelector(".side_bar");
-  sidebarSection.style.display = "none";
+  sidebarSection.style.width = "0";
+  let rightSection = document.getElementById("rightSection");
+  rightSection.classList.remove("col-lg-10");
+  rightSection.classList.add("col-lg-12");
 };
