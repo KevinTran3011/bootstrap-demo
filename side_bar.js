efbc0a0c9a390670a1beef2be18caf5f37c403ac
KevinTran3011 <tuankiettran30112000@gmail.com>
2023-12-18 17:15:12 +0700

    fixed the css for the signup box

--- side_bar.js
+++ side_bar.js
@@ -1,12 +1,14 @@
 const showSidebar = () => {
   let width = window.innerWidth;
+  const sidebarSection = document.querySelector(".side_bar");
+  sidebarSection.style.transition = "all 0.2s ease-in-out";
   if (width < 768) {
-    const sidebarSection = document.querySelector(".side_bar");
-    sidebarSection.style.transition = "all 0.2s ease-in-out";
     sidebarSection.style.width = "50%";
-    const closeButton = document.getElementById("closeButton");
-    closeButton.style.display = "block";
+  } else {
+    sidebarSection.style.width = "15%";
   }
+  const closeButton = document.getElementById("closeButton");
+  closeButton.style.display = "block";
 };
 
 const hideSidebar = () => {
