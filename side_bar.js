8217ac9f2202d1d54d755e559fadde320fc2a81a
KevinTran3011 <tuankiettran30112000@gmail.com>
2023-12-18 17:24:36 +0700

    fixed the side bar opening for when the width is full

--- side_bar.js
+++ side_bar.js
@@ -2,10 +2,13 @@ const showSidebar = () => {
   let width = window.innerWidth;
   const sidebarSection = document.querySelector(".side_bar");
   sidebarSection.style.transition = "all 0.2s ease-in-out";
+  let rightSection = document.getElementById("rightSection");
   if (width < 768) {
     sidebarSection.style.width = "50%";
   } else {
-    sidebarSection.style.width = "15%";
+    sidebarSection.style.width = "14.5%";
+    rightSection.classList.remove("col-lg-12");
+    rightSection.classList.add("col-lg-10");
   }
   const closeButton = document.getElementById("closeButton");
   closeButton.style.display = "block";
